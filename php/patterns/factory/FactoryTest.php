716fff81c97385c0dab546283688b27a2e79d998
Carlos Costa <carllos.nc@gmail.com>
2022-07-30 16:08:50 -0300

    adding pest

---
+++ php/patterns/factory/FactoryTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace patterns\factory\Factory;
+
+abstract class Creator
+{
+    abstract public function factoryMethod(): Product;
+
+    public function operation(): string
+    {
+        $product = $this->factoryMethod();
+
+        $result = "Cretor: The same creator's code has just worket with" . $product->operation();
+
+        return $result;
+    }
+}
+
+interface Product
+{
+    public function operation(): string;
+}
+
+class ConcreteCreator1 extends Creator implements Product
+{
+    public function operation(): string
+    {
+        return "{ConcreteProduct1}";
+    }
+
+    public function factoryMethod(): Product
+    {
+        return new ConcreteCreator1();
+    }
+}
+
+class ConcreteCreator2 extends Creator implements Product
+{
+    public function operation(): string
+    {
+        return "{ConcreteProduct2}";
+    }
+
+    public function factoryMethod(): Product
+    {
+        return new ConcreteCreator2();
+    }
+}
+
+function clientCode(Creator $creator)
+{
+    return "Client: " . $creator->operation();
+}
+
+test('Check concrete product 1', function () {
+    expect(clientCode(new ConcreteCreator1()))->toBe('Client: {ConcreteProduct1}');
+});
+
+test('Check concrete product 2', function () {
+    expect(clientCode(new ConcreteCreator2()))->toBe('Client: {ConcreteProduct2}');
+});
