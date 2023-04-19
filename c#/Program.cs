731e354317c72a3365eccf90fad5c37afc56ba3e
Carlos Costa <carllos.nc@gmail.com>
2023-04-19 11:30:58 -0300

    new notes

--- c#/Program.cs
+++ c#/Program.cs
@@ -1,8 +1,24 @@
-﻿class Program
+﻿//person class with name and age
+
+class Person
+{
+  public string? name;
+  public int age;
+
+  public Person() { }
+}
+
+class Program
 {
   static void Main(string[] args)
   {
-    int[] array = new int[5];
-    System.Console.WriteLine(array[0]);
+    Person person = new Person()
+    {
+      name = "John",
+      age = 20
+    };
+
+    System.Console.WriteLine(person.name);
+    System.Console.WriteLine(person.age);
   }
 }
